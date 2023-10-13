abe8d671b81facd25bd4af490596ef3b00556272
Adisak Rungruang <adisak01234@gmail.com>
2023-10-13 15:02:08 +0700

    add post and tests

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ import logging
 
 from fastapi import FastAPI
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 
@@ -13,6 +13,8 @@ def create_application() -> FastAPI:
     application = FastAPI()
 
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix='/summaries', tags=['summaries'])
+
     return application
 
 
--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -2,6 +2,7 @@ import os
 
 import pytest
 from starlette.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.main import create_application
 from app.config import get_setting, Settings
@@ -15,6 +16,21 @@ def get_setting_override():
 def test_app():
     app = create_application()
     app.dependency_overrides[get_setting] = get_setting_override
+    with TestClient(app) as test_client:
+        yield test_client
+
+
+@pytest.fixture(scope='module')
+def test_app_with_db():
+    app = create_application()
+    app.dependency_overrides[get_setting] = get_setting_override
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
     with TestClient(app) as test_client:
         yield test_client
 
